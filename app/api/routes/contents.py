cabc84cfcc04ac2de34e0404c25469c5534f49d0
gansekou <dev@gansekou.com>
2026-08-18 12:19:29 +0200

    Update contents.py

--- app/api/routes/contents.py
+++ app/api/routes/contents.py
@@ -2,7 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query 
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload 
 
 from app.database.session import get_db
 from app.schemas.content import ContentCreate, ContentResponse
